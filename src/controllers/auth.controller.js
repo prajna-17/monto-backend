b3411c1705ec97c2534a171371de30475d9a3a80
apoorva nayak <nayakapoorva2010@gmail.com>
2025-12-11 17:13:12 +0530

    resolve auth controller conflict

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { User } from "../models/user.model.js";
 import { createResponse, ErrorResponse } from "../utils/responseWrapper.js";
 
@@ -7,36 +7,36 @@ export const register = async (req, res) => {
     const { name, email, password } = req.body;
 
     const exists = await User.findOne({ email });
-    if (exists) return res.status(400).json(ErrorResponse(400,"User already exist"));
+    if (exists)
+      return res.status(400).json(ErrorResponse(400, "User already exist"));
 
     const hashed = await bcrypt.hash(password, 10);
 
     const user = await User.create({ name, email, password: hashed });
 
-   return  res.status(201).json(createResponse(201,user,"user create successfully"));
+    return res
+      .status(201)
+      .json(createResponse(201, user, "user create successfully"));
   } catch (error) {
-    return res.status(500).json(ErrorResponse(500,"internal server error"));
+    return res.status(500).json(ErrorResponse(500, "internal server error"));
   }
 };
-
-
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(404).json(ErrorResponse(400,"User Not Found"));
-
+    if (!user)
+      return res.status(404).json(ErrorResponse(400, "User Not Found"));
 
     const match = await bcrypt.compare(password, user.password);
 
-    if (!match) return res.status(400).json(ErrorResponse(400,"Invalid credential"));
+    if (!match)
+      return res.status(400).json(ErrorResponse(400, "Invalid credential"));
 
-    
-
-    return res.status(200).json(createResponse(200,user,{}));
+    return res.status(200).json(createResponse(200, user, {}));
   } catch (error) {
-    return res.status(500).json(ErrorResponse(500,"internal server error"));
+    return res.status(500).json(ErrorResponse(500, "internal server error"));
   }
 };
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import authRouter from "./routes/auth.routes.js";
 import categoryRouter from "./routes/category.routes.js";
 import productRouter from "./routes/product.routes.js";
 
-import swaggerUi from "swagger-ui-express"
+import swaggerUi from "swagger-ui-express";
 import { readFileSync } from "fs";
 // import swaggerDocument  from "./swagger-output.json" assert { type: 'json' };
 import { connectDB } from "./config/db.js";
@@ -16,29 +16,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/auth", authRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
-
 
 const swaggerDocument = JSON.parse(
   readFileSync(new URL("./swagger-output.json", import.meta.url), "utf-8")
 );
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("server Error",error)
-        
-    })
-    app.listen(process.env.PORT || 3000,()=>{
-        console.log(`server is listning on port ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log('MONGODB Connection FAILED:',err)
-})
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("server Error", error);
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is listning on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB Connection FAILED:", err);
+  });
